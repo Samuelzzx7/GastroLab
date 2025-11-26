f4a9906ff7062416ccf7727bcc5cfa5d782a62c6
tahpxz <thauanyv749@gmail.com>
2025-11-26 16:09:53 -0300

    Update java.js

--- Code/java.js
+++ Code/java.js
@@ -1,1 +1,13 @@
- 
+function buscarReceita() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let lista = document.querySelectorAll("#listaReceitas li");
+
+    lista.forEach(item => {
+        let texto = item.textContent.toLowerCase();
+        if (texto.includes(input)) {
+            item.style.display = "";
+        } else {
+            item.style.display = "none";
+        }
+    });
+} 
