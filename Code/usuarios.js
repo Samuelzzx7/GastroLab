17f410bf9a7974d0e0d347f01e05d25e8901b055
samuelzzx7 <samuelbarreto203040@gmail.com>
2025-11-29 19:24:43 -0300

    lista de ingredientes funcional

--- Code/usuarios.js
+++ Code/usuarios.js
@@ -5,3 +5,24 @@ function carregarConteudo(pagina) {
             document.querySelector(".conteudo").innerHTML = html;
         });
 }
+
+function addTarefa() {
+            let input = document.getElementById("tarefaInput");
+            let texto = input.value.trim();
+
+            if (texto === "") return;
+
+            let ul = document.getElementById("listaTarefas");
+
+            let li = document.createElement("li");
+            li.innerHTML = `${texto} <span class="remove" onclick="remover(this)">✖</span>`;
+
+            ul.appendChild(li);
+
+            input.value = "";  
+            input.focus();     
+        }
+
+        function remover(item) {
+            item.parentElement.remove();
+        }       
